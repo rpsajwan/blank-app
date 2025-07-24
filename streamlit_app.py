36668823aaf13fa23bdd50ceb5689247aab4aa31
rpsajwan <38873237+rpsajwan@users.noreply.github.com>
2025-07-24 10:40:55 +0000

    chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 st.title("🎈 My new Streamlit app third change")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
